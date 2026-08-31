49d04168148f6ad993c2a6a0d22fec5fc96e3caf
skwiera_magic <mckskwiera@gmail.com>
2026-08-31 21:25:42 +0100

    scanning and cleaning implemented

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanned := scanner.Scan()
+		if scanned == false {
+			scanner.Err()
+			break
+		}
+		texted := scanner.Text()
+		cleaned := cleanInput(texted)
+		fmt.Printf("Your command was: %s\n", cleaned[0])
+	}
 }
