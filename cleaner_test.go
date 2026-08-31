598d01f9d7200535c0be22258b3ceacc023bcc5b
skwiera_magic <mckskwiera@gmail.com>
2026-08-31 15:44:43 +0100

    initial commit

---
+++ cleaner_test.go
@@ -0,0 +1,47 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+    cases := []struct {
+	input    string
+	expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+			input:    "Charmander    Bulbasaur     PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+			input:    "",
+			expected: []string{},
+		},
+	}
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice
+		// if they don't match, use t.Errorf and continue to the next case
+		if len(actual) != len(c.expected) {
+			t.Errorf("actual has different length than expected")
+			continue
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("%s is not the same as %s", word, expectedWord)
+				t.Fail()
+			}
+		}
+	}
+}
